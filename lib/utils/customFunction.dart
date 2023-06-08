bbc330e3f5e06bf0a3db6b4654b9f891637ae9b6
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-06-09 00:19:06 +0100

    fixed navigation drawer

---
+++ lib/utils/customFunction.dart
@@ -0,0 +1,39 @@
+  import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<Placemark> GetAddressFromPosition(Position position) async {
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    Placemark place = placemark[0];
+    return place;
+  }
