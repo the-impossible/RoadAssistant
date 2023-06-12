068932e9d66377993db7f0fc10b9f1d0434d74c6
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-06-12 05:09:01 +0100

    :fire: fixed login page

--- lib/views/onBoarding/login.dart
+++ lib/views/onBoarding/login.dart
@@ -25,10 +25,9 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-        // width: MediaQuery.of(context).size.width,
-        // height: MediaQuery.of(context).size.height,
         color: Colors.white,
         child: Stack(
           children: [
@@ -36,46 +35,46 @@ class _LoginState extends State<Login> {
               padding: const EdgeInsets.only(top: 50.0),
               child: SingleChildScrollView(
                 child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
+                  width: size.width,
                   height: 500,
                   child: Form(
                     key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(
-                            top: 20.0,
-                          ),
-                          child: Text(
-                            'Sign in',
-                            style: TextStyle(
-                              fontFamily: 'Schyuler',
-                              fontSize: 30.0,
-                              color: Colors.blueAccent,
-                              fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              top: 20.0,
+                            ),
+                            child: Text(
+                              'Sign in',
+                              style: TextStyle(
+                                fontFamily: 'Schyuler',
+                                fontSize: 30.0,
+                                color: Colors.blueAccent,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
-                        ),
-                        Material(
-                          elevation: 10.0,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(200.0)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Image.asset(
-                              'assets/images/profile.png',
-                              width: 100,
-                              height: 100,
+                          Material(
+                            elevation: 10.0,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(200.0)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image.asset(
+                                'assets/images/profile.png',
+                                width: 100,
+                                height: 100,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 02,
-                        ),
-                        SizedBox(
-                          width: 420,
-                          child: inputField(
+                          const SizedBox(
+                            height: 02,
+                          ),
+                          inputField(
                               validator: FormValidation.validateEmail,
                               formController: loginController.emailController,
                               fieldIcon: const Icon(
@@ -84,10 +83,7 @@ class _LoginState extends State<Login> {
                               ),
                               hintText: 'Email',
                               obscure: false),
-                        ),
-                        SizedBox(
-                          width: 420,
-                          child: inputField(
+                          inputField(
                             fieldIcon:
                                 const Icon(Icons.lock, color: Colors.black),
                             hintText: 'Password',
@@ -101,66 +97,66 @@ class _LoginState extends State<Login> {
                                   : Icons.visibility_off),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 330,
-                          height: 50,
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.blueAccent),
-                              // shape:MaterialStateProperty.all(outlinedBorder: OutlinedBorder(10.0)),
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(15.0))),
-                            ),
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                // Call login controller
-                                loginController.verifyLogin();
-                              }
-                            },
-                            child: const Text(
-                              'Login',
-                              style: TextStyle(
-                                  fontSize: 30.0, fontFamily: 'Schyuler'),
+                          SizedBox(
+                            width: size.width * .9,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.blueAccent),
+                                // shape:MaterialStateProperty.all(outlinedBorder: OutlinedBorder(10.0)),
+                                shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(15.0))),
+                              ),
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  // Call login controller
+                                  loginController.verifyLogin();
+                                }
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontSize: 30.0, fontFamily: 'Schyuler'),
+                              ),
                             ),
                           ),
-                        ),
-                        InkWell(
-                          onTap: (() {}),
-                          child: const Text('Forget password?',
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 17.0,
-                                  fontFamily: 'Schyuler')),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Get.toNamed(Routes.registerPage);
-                          },
-                          child: const Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                    text: 'Not a member?',
-                                    style: TextStyle(
-                                        fontSize: 17.0,
-                                        fontFamily: 'Schyuler')),
-                                TextSpan(
-                                  text: 'Sign Up',
-                                  style: TextStyle(
-                                    fontSize: 19.0,
-                                    fontFamily: 'Schyuler',
+                          InkWell(
+                            onTap: (() {}),
+                            child: const Text('Forget password?',
+                                style: TextStyle(
                                     color: Colors.blue,
+                                    fontSize: 17.0,
+                                    fontFamily: 'Schyuler')),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Get.toNamed(Routes.registerPage);
+                            },
+                            child: const Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: 'Not a member?',
+                                      style: TextStyle(
+                                          fontSize: 17.0,
+                                          fontFamily: 'Schyuler')),
+                                  TextSpan(
+                                    text: 'Sign Up',
+                                    style: TextStyle(
+                                      fontSize: 19.0,
+                                      fontFamily: 'Schyuler',
+                                      color: Colors.blue,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
